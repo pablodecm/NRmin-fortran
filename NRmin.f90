27085ce7370c4a9cf41961c51212cf4510561cc9
pablodecm <pablodecm@gmail.com>
2015-01-07 20:56:36 +0100

    Setup basic program and module skeleton

---
+++ NRmin.f90
@@ -0,0 +1,12 @@
+PROGRAM NRmin
+
+! Fortran program to perform a non-linear fitting by least-square
+! minimization using Newton-Raphson method. In addition, an estimation
+! of the bias and uncertainty is carried out by toy MC.
+
+! Most of the code has been implemented as subroutines of NRmin_mod
+USE NRmin_mod
+
+IMPLICIT NONE
+
+END PROGRAM NRmin
---
+++ NRmin_mod.f90
@@ -0,0 +1,12 @@
+MODULE NRmin_mod
+
+! Module used by the NRmin program to perform a non-linear fitting
+! by Newton-Raphson method. Also is used to estimate bias and error
+! using toy MC.
+
+IMPLICIT NONE
+
+CONTAINS
+
+END MODULE NRmin_mod
+
