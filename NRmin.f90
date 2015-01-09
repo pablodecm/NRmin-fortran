8319fcece6330b71ed57e34b8f7ac136d6771ece
pablodecm <pablodecm@gmail.com>
2015-01-09 18:48:06 +0100

    Add goodness of fit

--- NRmin.f90
+++ NRmin.f90
@@ -6,6 +6,7 @@ PROGRAM NRmin
 
 ! Most of the code has been implemented as subroutines of NRmin_mod
 USE NRmin_mod
+USE mod_chi2
 
 IMPLICIT NONE
 
@@ -25,5 +26,10 @@ cov = cov_func(x, y, sigma, par)
 WRITE(*,*) "--- NR minimization final results --- "
 WRITE(*,"(A,F7.5,A,F7.5)") "a parameter: ", par(1,1), " \pm ", SQRT(cov(1,1))
 WRITE(*,"(A,F7.5,A,F7.5)") "b parameter: ", par(2,1), " \pm ", SQRT(cov(2,2))
+WRITE(*,*) "--- goodness of fit ---"
+WRITE(*,"(A,F5.2)") "Chi^2 value ", chi2_func(x, y, sigma, par)
+WRITE(*,"(A,I3)") "Degrees of freedom: ", SIZE(x) - SIZE(par)
+WRITE(*,"(A,F5.3)") "Probability chi^2(real)>chi^2(obtained): " &
+    & , gammq(REAL(SIZE(x)-SIZE(par)), chi2_func(x,y,sigma,par))
 
 END PROGRAM NRmin
--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 gfortran -c NRmin_mod.f90
+gfortran -c mod_chi2.f90
 gfortran -c NRmin.f90
-gfortran NRmin.o NRmin_mod.o -o NRmin.bin
+gfortran NRmin.o NRmin_mod.o mod_chi2.o -o NRmin.bin
